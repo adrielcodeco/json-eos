8433cc0c300ca9822a848038bd72948378996a29
Codeco <adrielcodeco@hotmail.com>
2023-03-22 10:55:49 -0300

    feat: github action

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,9 +14,9 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/json-eos/',
 
-  organizationName: 'co2-lab',
+  organizationName: 'adrielcodeco',
   projectName: 'json-eos',
 
   onBrokenLinks: 'throw',
