c20e9214bbad35e08c21183bb5ee4722d501ce9e
Codeco <adrielcodeco@hotmail.com>
2023-03-22 11:47:46 -0300

    fix: links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://adrielcodeco.github.io/json-eos/spec',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/json-eos/',
+  baseUrl: '/',
 
   organizationName: 'adrielcodeco',
   projectName: 'json-eos',
